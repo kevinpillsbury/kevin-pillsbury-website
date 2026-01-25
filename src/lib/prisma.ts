750c7c0dba25a6a57ace4b7df07b5da22655fbb5
kpillsbury <kpillsbu@hamilton.edu>
2026-01-24 23:39:02 -0500

    Fix prisma client imports andadd prisma generation in build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 // src/lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma/client'
 
 // Add prisma to the NodeJS global type
 interface CustomNodeJsGlobal extends NodeJS.Global {
@@ -9,8 +9,14 @@ interface CustomNodeJsGlobal extends NodeJS.Global {
 // Prevent multiple instances of Prisma Client in development
 declare const global: CustomNodeJsGlobal
 
-const prisma = global.prisma || new PrismaClient()
+const prisma = 
+  global.prisma || 
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  })
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
+if (process.env.NODE_ENV === 'development') {
+  global.prisma = prisma
+}
 
 export default prisma
