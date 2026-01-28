b7c8d181fa301ad4155fe1257d1400d5813fe825
kpillsbury <kpillsbu@hamilton.edu>
2026-01-28 13:36:09 -0500

    Expand max horizontal width of UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[var(--background)] text-[var(--foreground)]`}
       >
         <Navbar />
-        <main className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <main className="w-full px-4 sm:px-6 lg:px-8 py-8">
           {children}
         </main>
       </body>
