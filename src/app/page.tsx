2e87008a8f88b846e1f6685d5692388fa7429f0d
kpillsbury <kpillsbu@hamilton.edu>
2026-01-28 14:24:38 -0500

    Udapte one color theme and apply themes across whole website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center text-center min-h-[calc(100vh-8rem)] -mx-4 sm:-mx-6 lg:-mx-8 px-4 sm:px-6 lg:px-8">
       <div className="max-w-2xl">
-        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl font-serif">
+        <h1 className="text-4xl font-bold tracking-tight text-[var(--text-borders)] sm:text-6xl font-serif">
           Kevin Pillsbury
         </h1>
       </div>
