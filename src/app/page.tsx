db82d3802e50261ab3b57fbf3c6ecf3db55c6251
Kevin Pillsbury <kevin.pillsbury7@gmail.com>
2026-02-06 12:22:08 -0500

    Move crab back (behind text)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
         className="fixed top-16 inset-x-0 bottom-0 -z-10 bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: "url(/images/cartoon-space.jpg)" }}
       />
-      {/* Bouncing crab - z-20 so it's clickable above content */}
-      <div className="fixed top-16 inset-x-0 bottom-0 z-20 overflow-hidden">
+      {/* Bouncing crab - behind text */}
+      <div className="fixed top-16 inset-x-0 bottom-0 z-10 overflow-hidden">
         <BouncingCrab />
       </div>
-      {/* Content overlay */}
-      <div className="relative z-10 flex flex-col items-center justify-center text-center min-h-[calc(100vh-8rem)] -mx-4 sm:-mx-6 lg:-mx-8 px-4 sm:px-6 lg:px-8">
+      {/* Content overlay - in front of crab */}
+      <div className="relative z-20 flex flex-col items-center justify-center text-center min-h-[calc(100vh-8rem)] -mx-4 sm:-mx-6 lg:-mx-8 px-4 sm:px-6 lg:px-8 pointer-events-none">
         <div className="max-w-2xl">
           <h1 className="text-3xl font-bold tracking-tight text-[var(--text-borders)] sm:text-5xl font-serif">
             Kevin Pillsbury
